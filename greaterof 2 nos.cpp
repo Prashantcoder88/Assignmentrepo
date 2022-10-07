ca0bd87dd5ed9c307ab9d9486bfd3be541b9d0d2
Prashantcoder88 <115200889+Prashantcoder88@users.noreply.github.com>
2022-10-07 08:24:20 +0530

    Add files via upload

---
+++ greaterof 2 nos.cpp
@@ -0,0 +1,23 @@
+// wap a c++ program to find greater of 2 numbers.
+
+#include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+	 int a, b;
+	 cout<<"enter first no. a  "<<endl;
+	 cin>>a;
+	 
+	 cout<<"enter second no. b  "<<endl;
+	 cin>>b;
+	 if(a>b)
+	 {
+		 cout<<"a is greater than b";
+	 }
+	 else
+	 { 	
+		cout<<"b is greater than a";
+	 }
+	 return 0;
+ }
